12cf7429f1ab947b33b0b79fca7d087b3f6472d1
NILOIDE <nil@medis.nl>
2021-10-30 17:23:37 +0200

    -

--- torch_utils/ops/upfirdn2d.py
+++ torch_utils/ops/upfirdn2d.py
@@ -21,7 +21,7 @@ from . import conv2d_gradfix
 _plugin = None
 
 def _init():
-    # return False
+    return False
     global _plugin
     if _plugin is None:
         _plugin = custom_ops.get_plugin(
