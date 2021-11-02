8813587b2f99d377446f5d0a14a2babf27597dc2
NILOIDE <nil@medis.nl>
2021-11-02 23:41:10 +0100

    Copied over global _plugin try-except from stylegan2-ada

--- torch_utils/ops/bias_act.py
+++ torch_utils/ops/bias_act.py
@@ -9,9 +9,11 @@
 """Custom PyTorch ops for efficient bias and activation."""
 
 import os
+import warnings
 import numpy as np
 import torch
 import dnnlib
+import traceback
 
 from .. import custom_ops
 from .. import misc
@@ -38,14 +40,18 @@ _null_tensor = torch.empty([0])
 def _init():
     global _plugin
     if _plugin is None:
-        _plugin = custom_ops.get_plugin(
-            module_name='bias_act_plugin',
-            sources=['bias_act.cpp', 'bias_act.cu'],
-            headers=['bias_act.h'],
-            source_dir=os.path.dirname(__file__),
-            extra_cuda_cflags=['--use_fast_math'],
-        )
-    return True
+        try:
+            _plugin = custom_ops.get_plugin(
+                module_name='bias_act_plugin',
+                sources=['bias_act.cpp', 'bias_act.cu'],
+                headers=['bias_act.h'],
+                source_dir=os.path.dirname(__file__),
+                extra_cuda_cflags=['--use_fast_math'],
+            )
+        except:
+            warnings.warn(
+                'Failed to build CUDA kernels for bias_act. Falling back to slow reference implementation. Details:\n\n' + traceback.format_exc())
+    return _plugin is not None
 
 #----------------------------------------------------------------------------
 
