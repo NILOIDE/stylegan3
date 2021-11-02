5ba1c74c20f55bfa18f69824a62d3d31b6cc141d
NILOIDE <nil@medis.nl>
2021-11-02 23:43:44 +0100

    Copied over global _plugin try-except from stylegan2-ada

--- torch_utils/ops/filtered_lrelu.py
+++ torch_utils/ops/filtered_lrelu.py
@@ -23,14 +23,18 @@ _plugin = None
 def _init():
     global _plugin
     if _plugin is None:
-        _plugin = custom_ops.get_plugin(
-            module_name='filtered_lrelu_plugin',
-            sources=['filtered_lrelu.cpp', 'filtered_lrelu_wr.cu', 'filtered_lrelu_rd.cu', 'filtered_lrelu_ns.cu'],
-            headers=['filtered_lrelu.h', 'filtered_lrelu.cu'],
-            source_dir=os.path.dirname(__file__),
-            extra_cuda_cflags=['--use_fast_math'],
-        )
-    return True
+        try:
+            _plugin = custom_ops.get_plugin(
+                module_name='filtered_lrelu_plugin',
+                sources=['filtered_lrelu.cpp', 'filtered_lrelu_wr.cu', 'filtered_lrelu_rd.cu', 'filtered_lrelu_ns.cu'],
+                headers=['filtered_lrelu.h', 'filtered_lrelu.cu'],
+                source_dir=os.path.dirname(__file__),
+                extra_cuda_cflags=['--use_fast_math'],
+            )
+        except:
+            warnings.warn(
+                'Failed to build CUDA kernels for filtered_lrelu. Falling back to slow reference implementation. Details:\n\n' + traceback.format_exc())
+    return _plugin is not None
 
 def _get_filter_size(f):
     if f is None:
